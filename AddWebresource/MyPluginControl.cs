2bb5658f7dbcb3cb7f8d3dab9cc1378f1f3b2359
whoppacfm <13801775+whoppacfm@users.noreply.github.com>
2022-09-26 13:05:13 +0200

    Add files via upload

---
+++ AddWebresource/MyPluginControl.cs
@@ -0,0 +1,178 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using XrmToolBox.Extensibility;
+using Microsoft.Xrm.Sdk.Query;
+using Microsoft.Xrm.Sdk;
+using McTools.Xrm.Connection;
+using Microsoft.Crm.Sdk.Messages;
+
+namespace AddWebresource
+{
+    public partial class MyPluginControl : PluginControlBase
+    {
+        private Settings mySettings;
+
+        class WebresourceItem
+        {
+            public string name { get; set; }
+            public Guid id { get; set; }
+        }
+
+
+        public MyPluginControl()
+        {
+            InitializeComponent();
+        }
+
+        private void MyPluginControl_Load(object sender, EventArgs e)
+        {
+            ShowInfoNotification("This is a notification that can lead to XrmToolBox repository", new Uri("https://github.com/MscrmTools/XrmToolBox"));
+            
+            // Loads or creates the settings for the plugin
+            if (!SettingsManager.Instance.TryLoad(GetType(), out mySettings))
+            {
+                mySettings = new Settings();
+
+                LogWarning("Settings not found => a new settings file has been created!");
+            }
+            else
+            {
+                LogInfo("Settings found and loaded");
+            }
+        }
+
+        private void tsbClose_Click(object sender, EventArgs e)
+        {
+            CloseTool();
+        }
+
+        private void tsbSample_Click(object sender, EventArgs e)
+        {
+            // The ExecuteMethod method handles connecting to an
+            // organization if XrmToolBox is not yet connected
+            ExecuteMethod(GetAccounts);
+        }
+
+        private void GetAccounts()
+        {
+
+
+        }
+
+        /// <summary>
+        /// This event occurs when the plugin is closed
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void MyPluginControl_OnCloseTool(object sender, EventArgs e)
+        {
+            // Before leaving, save the settings
+            SettingsManager.Instance.Save(GetType(), mySettings);
+        }
+
+        /// <summary>
+        /// This event occurs when the connection has been updated in XrmToolBox
+        /// </summary>
+        public override void UpdateConnection(IOrganizationService newService, ConnectionDetail detail, string actionName, object parameter)
+        {
+            base.UpdateConnection(newService, detail, actionName, parameter);
+
+            if (mySettings != null && detail != null)
+            {
+                mySettings.LastUsedOrganizationWebappUrl = detail.WebApplicationUrl;
+                LogInfo("Connection has changed to: {0}", detail.WebApplicationUrl);
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            listBox1.DisplayMember = "name";
+            listBox1.ValueMember = "id";
+
+            listBox1.Items.Clear();
+            WorkAsync(new WorkAsyncInfo
+            {
+                Message = "Getting webresources",
+                Work = (worker, args) =>
+                {
+                    string fetchXml = @"<fetch version='1.0' top='50' output-format='xml-platform' mapping='logical' distinct='false' >
+                        <entity name = 'webresource' >
+                            <all-attributes/>
+                            <filter type='and'>
+                            <condition attribute='name' value='%";
+                    fetchXml += textBox_search.Text;
+                    fetchXml+=@"%' operator='like'/>
+                            </filter>        
+                            <order attribute='name' descending='false' />
+                          </entity>
+                        </fetch>";
+
+                    /*
+                        < filter type = 'or' >
+                        < condition attribute = 'name' value = '%.js%' operator= 'like' />
+                        < condition attribute = 'name' value = '%.html%' operator= 'like' />
+                        </ filter >
+                    */
+
+                    FetchExpression fq = new FetchExpression(fetchXml);
+                    args.Result = Service.RetrieveMultiple(fq);
+                },
+                PostWorkCallBack = (args) =>
+                {
+                    if (args.Error != null)
+                    {
+                        MessageBox.Show(args.Error.ToString(), "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    }
+                    var result = args.Result as EntityCollection;
+                    if (result != null)
+                    {
+                        //MessageBox.Show($"Found {result.Entities.Count} accounts");
+                        foreach (var ent in result.Entities)
+                        {
+                            WebresourceItem item = new WebresourceItem();
+                            item.name = ent.GetAttributeValue<string>("name");
+                            item.id = ent.Id;
+                            listBox1.Items.Add(item);
+                        }
+                    }
+                }
+            });
+        }
+
+        private void button_add_Click(object sender, EventArgs e)
+        {
+            if (textBox_solution.Text.Length > 1 && listBox1.SelectedIndex > -1)
+            {
+                var sln = textBox_solution.Text;
+                Guid itemid = ((WebresourceItem)listBox1.Items[listBox1.SelectedIndex]).id;
+                //MessageBox.Show(itemid);
+                try
+                {
+                    AddSolutionComponentRequest req = new AddSolutionComponentRequest();
+                    req.SolutionUniqueName = textBox_solution.Text;
+                    req.ComponentId = itemid;
+                    req.ComponentType = 61; //Webresource
+                    req.AddRequiredComponents = true;
+                    AddSolutionComponentResponse resp = (AddSolutionComponentResponse)Service.Execute(req);
+                    MessageBox.Show("Success");
+                }catch(Exception ex)
+                {
+                    MessageBox.Show("Error: " + ex.Message);
+                }
+
+
+            }
+            else
+            {
+                MessageBox.Show("Please select a webresource from the list and insert the (unique) solution name");
+            }
+        }
+    }
+}
---
+++ AddWebresource/MyPluginControl.designer.cs
@@ -0,0 +1,178 @@
+﻿namespace AddWebresource
+{
+    partial class MyPluginControl
+    {
+        /// <summary> 
+        /// Variable nécessaire au concepteur.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Nettoyage des ressources utilisées.
+        /// </summary>
+        /// <param name="disposing">true si les ressources managées doivent être supprimées ; sinon, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Code généré par le Concepteur de composants
+
+        /// <summary> 
+        /// Méthode requise pour la prise en charge du concepteur - ne modifiez pas 
+        /// le contenu de cette méthode avec l'éditeur de code.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.toolStripMenu = new System.Windows.Forms.ToolStrip();
+            this.tsbClose = new System.Windows.Forms.ToolStripButton();
+            this.tssSeparator1 = new System.Windows.Forms.ToolStripSeparator();
+            this.tsbSample = new System.Windows.Forms.ToolStripButton();
+            this.textBox_search = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox_solution = new System.Windows.Forms.TextBox();
+            this.button_add = new System.Windows.Forms.Button();
+            this.toolStripMenu.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // toolStripMenu
+            // 
+            this.toolStripMenu.ImageScalingSize = new System.Drawing.Size(24, 24);
+            this.toolStripMenu.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.tsbClose,
+            this.tssSeparator1,
+            this.tsbSample});
+            this.toolStripMenu.Location = new System.Drawing.Point(0, 0);
+            this.toolStripMenu.Name = "toolStripMenu";
+            this.toolStripMenu.Padding = new System.Windows.Forms.Padding(0, 0, 2, 0);
+            this.toolStripMenu.Size = new System.Drawing.Size(1130, 27);
+            this.toolStripMenu.TabIndex = 4;
+            this.toolStripMenu.Text = "toolStrip1";
+            // 
+            // tsbClose
+            // 
+            this.tsbClose.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Text;
+            this.tsbClose.Name = "tsbClose";
+            this.tsbClose.Size = new System.Drawing.Size(107, 24);
+            this.tsbClose.Text = "Close this tool";
+            this.tsbClose.Click += new System.EventHandler(this.tsbClose_Click);
+            // 
+            // tssSeparator1
+            // 
+            this.tssSeparator1.Name = "tssSeparator1";
+            this.tssSeparator1.Size = new System.Drawing.Size(6, 27);
+            // 
+            // tsbSample
+            // 
+            this.tsbSample.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Text;
+            this.tsbSample.Name = "tsbSample";
+            this.tsbSample.Size = new System.Drawing.Size(57, 24);
+            this.tsbSample.Text = "Try me";
+            this.tsbSample.Click += new System.EventHandler(this.tsbSample_Click);
+            // 
+            // textBox_search
+            // 
+            this.textBox_search.Location = new System.Drawing.Point(126, 42);
+            this.textBox_search.Name = "textBox_search";
+            this.textBox_search.Size = new System.Drawing.Size(165, 22);
+            this.textBox_search.TabIndex = 5;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(297, 42);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(111, 23);
+            this.button1.TabIndex = 6;
+            this.button1.Text = "Search";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 16;
+            this.listBox1.Location = new System.Drawing.Point(16, 71);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.ScrollAlwaysVisible = true;
+            this.listBox1.Size = new System.Drawing.Size(1095, 228);
+            this.listBox1.TabIndex = 7;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(27, 45);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(93, 17);
+            this.label1.TabIndex = 8;
+            this.label1.Text = "Webresource";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(17, 321);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(59, 17);
+            this.label2.TabIndex = 10;
+            this.label2.Text = "Solution";
+            // 
+            // textBox_solution
+            // 
+            this.textBox_solution.Location = new System.Drawing.Point(82, 321);
+            this.textBox_solution.Name = "textBox_solution";
+            this.textBox_solution.Size = new System.Drawing.Size(195, 22);
+            this.textBox_solution.TabIndex = 9;
+            // 
+            // button_add
+            // 
+            this.button_add.Location = new System.Drawing.Point(16, 362);
+            this.button_add.Name = "button_add";
+            this.button_add.Size = new System.Drawing.Size(275, 41);
+            this.button_add.TabIndex = 11;
+            this.button_add.Text = "Add selected Webresource to Solution";
+            this.button_add.UseVisualStyleBackColor = true;
+            this.button_add.Click += new System.EventHandler(this.button_add_Click);
+            // 
+            // MyPluginControl
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.button_add);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.textBox_solution);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox_search);
+            this.Controls.Add(this.toolStripMenu);
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.Name = "MyPluginControl";
+            this.Size = new System.Drawing.Size(1130, 599);
+            this.Load += new System.EventHandler(this.MyPluginControl_Load);
+            this.toolStripMenu.ResumeLayout(false);
+            this.toolStripMenu.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.ToolStrip toolStripMenu;
+        private System.Windows.Forms.ToolStripButton tsbClose;
+        private System.Windows.Forms.ToolStripButton tsbSample;
+        private System.Windows.Forms.ToolStripSeparator tssSeparator1;
+        private System.Windows.Forms.TextBox textBox_search;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox_solution;
+        private System.Windows.Forms.Button button_add;
+    }
+}
